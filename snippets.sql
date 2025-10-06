abf0219f4d8824d42b5846cd94aafdeac53122b9
lucianareynaud <reynaud.luciana@gmail.com>
2025-10-06 09:47:59 -0300

    v0

--- snippets.sql
+++ snippets.sql
@@ -22,3 +22,22 @@ GROUP BY 1
 ORDER BY 1;
 
 
+--Add an additional column to your previous query that counts the number of clicks from the variant group and alias it as ‘variant_clicks’.
+
+
+
+
+SELECT modal_text,
+  COUNT(DISTINCT CASE
+    WHEN ab_group = 'control' THEN user_id
+    END) AS 'control_clicks',
+  COUNT(DISTINCT CASE
+    WHEN ab_group = 'variant' THEN user_id
+    END) AS 'variant_clicks'
+
+FROM onboarding_modals
+GROUP BY 1
+ORDER BY 1;
+
+
+
