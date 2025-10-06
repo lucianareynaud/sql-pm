91833d5801e966b506f151cfe5b0516c442ac5ac
lucianareynaud <reynaud.luciana@gmail.com>
2025-10-06 09:38:27 -0300

    v0

--- snippets.sql
+++ snippets.sql
@@ -2,3 +2,13 @@ SELECT question_text,
   COUNT(DISTINCT user_id)
 FROM survey_responses
 GROUP BY 1;
+
+
+--Liste todos os textos de modais de onboarding e mostre, para cada um, quantos usuários únicos o visualizaram, ordenando os resultados pelo nome do modal.
+
+SELECT modal_text, 
+COUNT(DISTINCT user_id) 
+FROM onboarding_modals 
+GROUP BY 1 
+ORDER BY 1;
+
